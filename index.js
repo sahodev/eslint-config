08a18c73ac1da681a75cb43195173d94c1f3d510
Sami Honkasalo <honkasalo.sami@gmail.com>
2022-09-04 10:56:32 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:eslint-comments/recommended",
+    "plugin:jest/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+    "plugin:promise/recommended",
+  ],
+  plugins: ["react", "jsx-a11y", "import", "@typescript-eslint"],
+  globals: {},
+  rules: {
+    // prettier
+    "prettier/prettier": ["error"],
+    // TypeScript
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/no-object-literal-type-assertion": "off",
+    "no-shadow": "off",
+    "import/no-cycle": "off",
+    "@typescript-eslint/no-shadow": ["error"],
+    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "error",
+    // Use function hoisting to improve code readability
+    "no-use-before-define": "off",
+    "no-void": "off",
+    "promise/always-return": "off",
+    // Allow most functions to rely on type inference. If the function is exported, then `@typescript-eslint/explicit-module-boundary-types` will ensure it's typed.
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      {
+        functions: false,
+        classes: true,
+        variables: true,
+        typedefs: true,
+      },
+    ],
+    "no-param-reassign": [
+      "error",
+      { props: true, ignorePropertyModificationsFor: ["state", "draft"] },
+    ],
+    // React
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "react/prop-types": ["off", {}],
+    "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/function-component-definition": "off",
+    // import
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        mjs: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+      node: {
+        extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
+      },
+    },
+    "import/extensions": [".js", ".ts", ".mjs", ".jsx", ".tsx"],
+    jest: {
+      version: 27,
+    },
+    react: {
+      version: "18.2.0",
+    },
+  },
+};
